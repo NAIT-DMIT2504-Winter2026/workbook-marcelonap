46c9a5d97caa73f940299480b5312bdc4afde62b
marcelonap <73085259+marcelonap@users.noreply.github.com>
2026-01-12 20:58:40 -0700

    Adding Temp Script

---
+++ what_is_the_temp/bin/what_is_the_temp.dart
@@ -0,0 +1,5 @@
+import 'package:what_is_the_temp/what_is_the_temp.dart' as what_is_the_temp;
+
+void main(List<String> arguments) {
+  print("Today's Temp is:  ${what_is_the_temp.getTemp()}!");
+}
---
+++ what_is_the_temp/lib/what_is_the_temp.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+int calculate() {
+  return 6 * 7;
+}
+
+int getTemp() {
+  return Random().nextInt(40) * -1;
+}
